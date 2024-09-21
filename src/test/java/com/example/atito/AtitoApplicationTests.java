dd7bca2a7a447f9dd388d8a65bc1875468b7c77f
Ирина Устинова <IUstinova@alfabank.ru>
2024-09-21 21:34:14 +0300

    Создала шаблон Spring приложения

---
+++ src/test/java/com/example/atito/AtitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
